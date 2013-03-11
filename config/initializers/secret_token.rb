4daa6567f7caa16644e2ed10ed91f64e29a408c1
Kerry Houle <khoule38@academic.rrc.ca>
2013-03-11 15:10:51 -0500

    New Scaffold Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsNewScaffold::Application.config.secret_token = '4d8f01f17aca6c433b5de19e11b2e47d1602896f02fe1632e24b6d58ff448dca0c95bf39ff27ca317a3897a823f54acbc56388aaf2b42c8cf65832994fe7aff2'
